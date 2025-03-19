a639993b701caa3515caade4803b5a5f0136fed8
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-19 06:08:53 +0530

    get data from product service and display product title on UI

--- ContosoCrafts.Website/Models/Product.cs
+++ ContosoCrafts.Website/Models/Product.cs
@@ -8,18 +8,6 @@ namespace ContosoCrafts.Website.Models
 	{
 		public Product()
 		{
-            //"Id": "selinazawacki-moon",
-            //"Maker": "selinazawacki",
-            //"img": "https://user-images.githubusercontent.com/41929050/61567057-142c1c80-aa33-11e9-9781-9e442418eaab.png",
-            //"Url": "https://www.instagram.com/p/BFktVYPinKQ/",
-            //"Title": "Holographic Dark Moon Necklace",
-            //"Description": "Not sure if I\u0027ll be making more, get it while I have it in the store.",
-            //"Ratings": null
-
-            
-
-            
-
         }
 
         public string Id { get; set; }
