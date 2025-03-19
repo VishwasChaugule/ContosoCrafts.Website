ad18c72937bd9939e7a7344cda3d92bbddf0b8c2
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-19 05:45:40 +0530

    Create ASP.NET core app and add model and service to get Product from json file

---
+++ ContosoCrafts.Website/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.Website.Models
+{
+	public class Product
+	{
+		public Product()
+		{
+            //"Id": "selinazawacki-moon",
+            //"Maker": "selinazawacki",
+            //"img": "https://user-images.githubusercontent.com/41929050/61567057-142c1c80-aa33-11e9-9781-9e442418eaab.png",
+            //"Url": "https://www.instagram.com/p/BFktVYPinKQ/",
+            //"Title": "Holographic Dark Moon Necklace",
+            //"Description": "Not sure if I\u0027ll be making more, get it while I have it in the store.",
+            //"Ratings": null
+
+            
+
+            
+
+        }
+
+        public string Id { get; set; }
+
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
+
---
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using ContosoCrafts.Website.Models;
+
+namespace ContosoCrafts.Website.Services
+{
+	public class JsonFileProductService
+	{
+		public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+		{
+			WebHostEnvironment = webHostEnvironment;
+		}
+
+		public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+		private string GetJsonFileName()
+		{
+			return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+		}
+
+		public IEnumerable<Product> GetProducts()
+		{
+			using(var jsonFileReader = File.OpenText(GetJsonFileName()))
+			{
+				return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+					new JsonSerializerOptions
+					{
+						PropertyNameCaseInsensitive = true
+					});
+
+            }
+			
+		}
+	}
+}
+
